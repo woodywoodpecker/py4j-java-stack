e6faa3e957e7749235fe2bc201cbbca986e01087
woodywoodpecker <1210513673@qq.com>
2019-04-22 23:53:27 +0800

    add component for vue and component template must have the root node

---
+++ javascript/src/main/resources/js/vue/component/my-1-component.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- https://vuejs.org/v2/guide/migration.html#Fragment-Instances-removed -->
+    <div>
+        <div>Man name: {{this.$parent.man.name}}</div>
+        <div>Counter: {{this.$parent.counter}}</div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
