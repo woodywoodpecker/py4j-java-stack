c87d217915119648c78d43ac6ef3c34684beb856
warrenpu <warrenpu@aaxiscommerce.com>
2019-05-27 18:30:41 +0800

    code fix to stable

--- search-engine/src/main/java/es/App.java
+++ search-engine/src/main/java/es/App.java
@@ -8,20 +8,34 @@ import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.IOException;
 
-public class App {
+public class App
+{
+    public static void main(String[] args) throws IOException
+    {
+        RestHighLevelClient client = null;
 
-    public static void main(String[] args) throws IOException {
-        final RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost",9200,"http"))
-        );
+        try {
+            client = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost("localhost",9200,"http"))
+            );
 
-        GetRequest request = new GetRequest("elastic_search_edge_batch");
+            GetRequest request = new GetRequest("elastic_search_edge_batch");
 
-        GetResponse response = client.get(request);
+            request.id("14");
 
-        System.out.println(response.getSource());
+            GetResponse response = client.get(request);
 
-        client.close();
+            System.out.println(response.getSource());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null)
+            {
+                client.close();
+            }
+        }
+
     }
 
 }
